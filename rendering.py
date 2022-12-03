bb4db7f1efe45f91e4416da88347680e46dc1487
roopekt <roope.keto-tokoi@outlook.com>
2022-12-03 16:22:45 +0200

    circles now drawn with a cross

--- rendering.py
+++ rendering.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass, field
-from physics.collision import Collider
+from physics.collision import Collider, CircleCollider
 from physics.Rigidbody import Rigidbody
 from physics.PhysicsWorld import PhysicsWorld
 from pygame import Color
 from pygame.math import Vector2
 import pygame
 from utility import zero_vector_factory
+import math
 
 @dataclass
 class Camera:
@@ -38,14 +39,34 @@ class RenderableWorld:
         pygame.display.flip()
 
     def draw_collider(self, collider: Collider, color: Color):
-        scale = self.camera.height / self.window.get_height()
-        position = (collider.position() - self.camera.position) / scale
-        position.y = -position.y
-        position += Vector2(self.window.get_width(), self.window.get_height()) / 2
+        if isinstance(collider, CircleCollider):
+            self.draw_circle_collider(collider, color)
+        else:
+            raise Exception(f"Can't draw a {type(collider)}.")
+    
+    def draw_circle_collider(self, collider: CircleCollider, color: Color):
+        collider_world_pos = collider.position()
 
-        #temporary
         pygame.draw.circle(
             self.window,
             color,
-            position,
-            collider.radius / scale)
+            self.get_screen_position(collider_world_pos),
+            collider.radius / self.get_scale())
+
+        # extra cross
+        orientation = collider.orientation()
+        straight_angle = math.pi / 2.0
+        beam_points = [Vector2(collider.radius, 0).rotate_rad(i * straight_angle + orientation) + collider_world_pos for i in range(4)] #world space
+        beam_points = [self.get_screen_position(p) for p in beam_points] #screen space
+        beam_color = Color(255, 255, 255) - color #negative color
+        pygame.draw.line(self.window, beam_color, beam_points[0], beam_points[2])
+        pygame.draw.line(self.window, beam_color, beam_points[1], beam_points[3])
+
+    def get_scale(self):
+        return self.camera.height / self.window.get_height()
+
+    def get_screen_position(self, world_position: Vector2):
+        pos = (world_position - self.camera.position) / self.get_scale()
+        pos.y = -pos.y
+        pos += Vector2(self.window.get_width(), self.window.get_height()) / 2
+        return pos
