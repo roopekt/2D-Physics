213ffac2793ad6e0b11e91c51ed96fedcd1135e4
roopekt <roope.keto-tokoi@outlook.com>
2022-12-13 20:23:59 +0200

    collision response: rotated coordinate system by 90 degrees clockwise (x = tangent, y = normal)

--- physics/collision_response.py
+++ physics/collision_response.py
@@ -1,28 +1,30 @@
 from pygame.math import Vector2
-from utility import divide_vectors_as_complex, multiply_vectors_as_complex
+from utility import divide_vectors_as_complex, multiply_vectors_as_complex, get_tangent
 import math
 from .bodies import Collision
 
 def update_velocities(collision: Collision):
+    tangent = get_tangent(collision.normal)
     relative_velocity: Vector2 = (collision.bodyB.rigidbody.velocity_at_point(collision.collision_point)
                                 - collision.bodyA.rigidbody.velocity_at_point(collision.collision_point))
-    relative_velocity = divide_vectors_as_complex(relative_velocity, collision.normal) #rotate so that x is in the direction of the normal
+    relative_velocity = divide_vectors_as_complex(relative_velocity, tangent) #rotate so that y is in the direction of the normal
     reaction_impulse = Vector2(0.0)
 
-    if relative_velocity.x > 0.0:
+    if relative_velocity.y > 0.0:
         return
 
     #bounce
     velocity_change_for_unit_impulse = (collision.bodyA.rigidbody.apply_test_impulse( collision.normal, collision.collision_point)
                                         + collision.bodyB.rigidbody.apply_test_impulse(-collision.normal, collision.collision_point))
     elasticity = max(collision.bodyA.elasticity, collision.bodyB.elasticity)
-    target_velocity_change = (1 + elasticity) * relative_velocity.x
-    reaction_impulse.x = target_velocity_change / velocity_change_for_unit_impulse
+    target_velocity_change = (1 + elasticity) * relative_velocity.y
+    reaction_impulse.y = target_velocity_change / velocity_change_for_unit_impulse
 
     #TODO: friction
 
+
     #apply the impulses
-    reaction_impulse_world_space = multiply_vectors_as_complex(reaction_impulse, collision.normal)
+    reaction_impulse_world_space = multiply_vectors_as_complex(reaction_impulse, tangent)
     collision.bodyA.rigidbody.apply_impulse_at_point( reaction_impulse_world_space, collision.collision_point)
     collision.bodyB.rigidbody.apply_impulse_at_point(-reaction_impulse_world_space, collision.collision_point)
 
